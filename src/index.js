a551a7b7eb582f17e849fa01a98ec8f3101647ec
Pavel Shestak <pavelshestak29@gmail.com>
2018-09-17 23:14:22 +0300

    'five_task'

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+
+    var currency_money = currency,
+        template_money = {"H": 50,"Q": 25,"D": 10,"N": 5,"P": 1},
+        output_obj = {},
+        currency_nominal, current_value;
+
+    for (type_money in template_money) {
+
+        currency_nominal = template_money[type_money];
+        current_value = Math.trunc(currency_money/currency_nominal);
+
+        if(current_value === 0){
+            continue;
+        }else{
+            output_obj[type_money] = current_value;
+            currency_money %= currency_nominal;
+        }
+
+    }
+
+    if (currency <= 0) {
+        return {};
+    }
+    if (currency >= 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }
+
+    return output_obj;
+};
